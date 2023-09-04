4a007bb37db72e93a6e9ba0bb115b1d63223ab20
Vinay <vinaywebguy@gmail.com>
2023-09-04 12:09:39 +0530

    Added more levels and results section at game end

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ let randomIndex = 0;
 let type = "";
 let ans_type;
 let start = false;
+let data = "";
 
 function generateEquation(equations, answers) {
+
     type = equations;
     ans_type = answers;
     randomIndex = Math.floor(Math.random() * equations.length);
@@ -18,17 +20,16 @@ function generateEquation(equations, answers) {
     $('#equation').html(eqn.replace('?', '<input type="number" id="userInput">'));
     $('#res').html(eqn.replace('?', '<span id="result"></span>'));
     $('#userInput').focus();
-    $('#userInput').on('keyup', function() {
+    $('#userInput').on('keyup', function () {
         let val = $(this).val();
         $('#result').html(val);
         $('#submit').toggleClass('hidden', val === '');
         if (!start) {
             start = true;
-            startTime();
+            updateTime();
         }
     });
-    renderScore();
-    renderSteps();
+   
 }
 
 function checkAns(data) {
@@ -39,13 +40,27 @@ function checkAns(data) {
     }
     steps++;
     removeEquation();
+    renderScore();
+    renderSteps();
     if (steps < 15) {
         generateEquation(easyEquations, easyAnswers);
-    } else if(steps < 25) {
+    } else if (steps < 25) {
         generateEquation(mediumEquations, mediumAnswers);
     }
-    else if (steps < 35) {
+    else if (steps < 40) {
         generateEquation(hardEquations, hardAnswers);
+    }
+    else if (steps < 50) {
+        generateEquation(moreHardEquations, moreHardAnswers);
+    }
+    else if (steps < 60) {
+        generateEquation(extremeEquations, extremeAnswers);
+    }
+    else if (steps < 70) {
+        generateEquation(moreExtremeEquations, moreExtremeAnswers);
+    }
+    else {
+        showResults();
     }
 }
 
@@ -53,32 +68,44 @@ function renderScore() {
     $('#score').html(score);
 }
 
+function showResults() {
+    $('#equation').remove()
+    $('#res').remove()
+    $('.results').addClass('show');
+    $('#score_result').html(score)
+    $('#steps_result').html(steps)
+    $('#time_result').html(time)
+    start = false;
+    updateTime();
+}
+
 function renderSteps() {
     $('#steps').html(steps);
 }
 
-function startTime() {
-    if (start) {
-        setInterval(() => {
+function updateTime() {
+    let timeInterval = setInterval(() => {
+        if (start) {
             $('#time').html(time);
             time++;
-        }, 1000);
-    }
+        }
+        else {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
 }
 
 function removeEquation() {
-        type.splice(randomIndex, 1);
-        ans_type.splice(randomIndex, 1);
+    type.splice(randomIndex, 1);
+    ans_type.splice(randomIndex, 1);
 }
 
 
 
-$('#submit').on('click', function() {
+$('#submit').on('click', function () {
     $('#submit').addClass('hidden');
-    let data = $('#res').text();
+    data = $('#res').text();
     checkAns(data);
-
-    console.log(type);
 });
 
 generateEquation(easyEquations, easyAnswers);
